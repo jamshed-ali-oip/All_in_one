d34838d5d2021d936817b9f86e2fe167b0a892cf
jamshed-ali-oip <105854101+jamshed-ali-oip@users.noreply.github.com>
2023-01-16 03:38:11 -0700

    customBtndne

---
+++ All_in_one/src/components/Mybutton.js
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View,TouchableOpacity,Dimensions } from 'react-native'
+import React from 'react'
+const { height, width } = Dimensions.get('window');
+const Mybutton = ({props}) => {
+  return (
+<TouchableOpacity
+style={{
+    backgroundColor:props?.backgroundColor,
+    height:props?.height,
+    width:props?.width,
+    alignSelf:"center",
+    justifyContent:"center",
+    borderRadius:props?.borderRadius
+
+    }}
+>
+    <Text
+    style={{
+        fontSize:props?.fontSize,
+        alignSelf:"center",
+        fontWeight:"bold",
+        color:"white"
+    }}
+    >
+        ok 
+    </Text>
+</TouchableOpacity>    
+  )
+}
+
+export default Mybutton
+
+const styles = StyleSheet.create({})
+// ***************************how to use**************************** 
+// backgroundColor 
+// height 
+// width 
+// alignSelf 
+// justifyContent
+// borderRadius,
+// fontSize
